960f338a352d6a1a92b32d241939768b56c2a88d
dgburn <dongbowen@rayclear.com>
2018-12-05 22:02:50 +0800

    add server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+var http = require('http')
+var fs = require('fs')
+
+var httpServer = http.createServer(function(req, res) {
+  var url = req.url === '/' ? 'index.html' : req.url
+
+  fs.readFile(`./${url}`, 'utf-8', function(err, data) {
+    if (err) {
+      res.write('404,您访问的页面不存在')
+      res.end()
+    } else {
+      res.write(data)
+      res.end()
+    }
+  })
+})
+
+httpServer.listen(3000)
