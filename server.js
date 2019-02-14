b41fc3fa3fbdfbb5c04f88367aa06849b1f278a0
dgburn <dongbowen@rayclear.com>
2019-02-14 10:15:54 +0800

    增加weapp-cropper的视频展示

--- server.js
+++ server.js
@@ -15,4 +15,18 @@ var httpServer = http.createServer(function(req, res) {
   })
 })
 
+var httpServer = http.createServer(function(req, res) {
+  var url = req.url === '/weapp-cropper-preview/' ? 'index.html' : req.url
+
+  fs.readFile(`./${url}`, 'utf-8', function(err, data) {
+    if (err) {
+      res.write('404,您访问的页面不存在')
+      res.end()
+    } else {
+      res.write(data)
+      res.end()
+    }
+  })
+})
+
 httpServer.listen(3000)
