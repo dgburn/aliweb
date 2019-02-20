8e1968840df03eb4746e82e0fb23bd39081498f9
dgburn <dongbowen@rayclear.com>
2019-02-20 19:31:14 +0800

    test server.js

--- server.js
+++ server.js
@@ -2,13 +2,18 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/' + 'index.html')
-})
+var index = require('./index.html')
+var cropper = require('./weapp-cropper-preview/index.html')
 
-app.get('/weapp-cropper', function (req, res) {
-  res.sendFile(__dirname + '/weapp-cropper-preview/' + 'index.html')
-})
+app.use('/', index)
 
-console.log('启动成功')
+// app.get('/', function (req, res) {
+//   res.sendFile(__dirname + '/' + 'index.html')
+// })
+
+// app.get('/weapp-cropper', function (req, res) {
+//   res.sendFile(__dirname + '/weapp-cropper-preview/' + 'index.html')
+// })
+
+console.log('start success')
 app.listen(3000)
