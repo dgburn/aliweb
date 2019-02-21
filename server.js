f3c677abc7a6f16f40cbea2b04af9ed20c705177
dgburn <dongbowen@rayclear.com>
2019-02-21 16:15:11 +0800

    fix

--- server.js
+++ server.js
@@ -1,19 +1,58 @@
 
-var express = require('express')
-var app = express()
+// var http = require("http");
+// var url = require('url')
+// var fs = require('fs')
 
-var index = require('./index.html')
-var cropper = require('./weapp-cropper-preview/index.html')
-
-app.use('/', index)
-
-// app.get('/', function (req, res) {
-//   res.sendFile(__dirname + '/' + 'index.html')
+// var app = http.createServer(function (req, res) {
+//   var path = url.parse(req.url).pathname
+//   console.log(path)
+//   const pathname = path.substr(1)
+//   console.log(pathname)
+//   if (pathname === 'index') {
+//     fs.readFile(`${__dirname}/index.html`, (err, data) => {
+//       if (err) {
+//         res.writeHead(404, {
+//           'Content-Type': 'text/html;charset=UTF-8'
+//         })
+//         res.write('read file failed')
+//       } else {
+//         res.writeHead(200, {
+//           'Content-Type': 'text/html;charset=UTF-8'
+//         })
+//         res.write(data)
+//       }
+//       res.end()
+//     })
+//   } else if (pathname === 'cropper') {
+//     fs.readFile(`${__dirname}/weapp-cropper-preview/index.html`, (err, data) => {
+//       if (err) {
+//         res.write('read file failed2')
+//       } else {
+//         res.writeHead(200, {
+//           'Content-Type': 'text/html;charset=UTF-8'
+//         })
+//         res.write(data.toString())
+//       }
+//     })
+//   }
+//   // var pathname = url.parse(req.url).pathname
+//   // res.writeHead(200, { "Content-type": "text/blain" });
+//   // res.write("Hello NodeJs");
+//   // res.end();
 // })
 
-// app.get('/weapp-cropper', function (req, res) {
-//   res.sendFile(__dirname + '/weapp-cropper-preview/' + 'index.html')
+// app.listen(4000, () => {
+//   console.log('server start success in port 4000')
 // })
 
-console.log('start success')
-app.listen(3000)
+let express = require("express");
+let app = express();
+ 
+app.get('/', function(req, res, next) {
+  res.sendFile(__dirname + '/index.html')
+})
+// app.use(express.static("weapp-cropper-preview"));//关键是这一句，我的目录是html的目录
+ 
+app.listen(3000, ()=>{
+  console.log("服务启动成功。");
+})
